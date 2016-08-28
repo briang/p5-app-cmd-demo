389063a553dddabf86c8184cd82f3ac2c6030905
brian greenfield <briang@cpan.org>
2016-08-28 10:13:00 +0100

    add command class

---
+++ lib/App/Kiwi.pm
@@ -0,0 +1,12 @@
+package App::Kiwi;
+
+use 5.022;
+
+use strict;
+use warnings;
+
+use Data::Dump;
+################################################################################
+use App::Cmd::Setup -app;
+
+1;
